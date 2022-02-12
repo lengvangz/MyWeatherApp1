1dc36834771162365a89163a109a4e2bec14c4d9
Leng Vang <lengvang1991@icloud.com>
2022-02-11 22:58:44 -0600

    Assignment 2

--- app/src/main/java/com/example/myweatherapp1/MainActivity.kt
+++ app/src/main/java/com/example/myweatherapp1/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.myweatherapp1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
